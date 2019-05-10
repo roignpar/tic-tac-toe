f450c12dff7d68f020c03104755818e1acd2250b
Robert Ignat <robert.ignat91@gmail.com>
2019-05-10 15:36:00 +0300

    Move game settings creation to separate fn

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -15,16 +15,16 @@ const WINDOW_HEIGHT: u32 = 650;
 fn main() -> QSResult<()> {
     let window_size = Vector::new(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    run::<TicTacToe>(
-        "Tic-tac-toe",
-        window_size,
-        Settings {
-            // no resizing
-            min_size: Some(window_size),
-            max_size: Some(window_size),
-            ..Settings::default()
-        },
-    );
+    run::<TicTacToe>("Tic-tac-toe", window_size, settings(window_size));
 
     Ok(())
+}
+
+fn settings(window_size: Vector) -> Settings {
+    Settings {
+        // no resizing
+        min_size: Some(window_size),
+        max_size: Some(window_size),
+        ..Settings::default()
+    }
 }
