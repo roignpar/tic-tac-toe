a932799d4a887d3e11ecebe4c2603644bbb28213
Robert Ignat <robert.ignat91@gmail.com>
2019-05-22 14:55:44 +0300

    Add font for buttons

--- tic-tac-toe/src/game_state/assets.rs
+++ tic-tac-toe/src/game_state/assets.rs
@@ -1,9 +1,13 @@
-use quicksilver::{graphics::Image, Result as QSResult};
+use quicksilver::{
+    graphics::{Font, Image},
+    Result as QSResult,
+};
 
 pub struct GameAssets {
     pub line: Image,
     pub x: Image,
     pub z: Image,
+    pub font: Font,
 }
 
 impl GameAssets {
@@ -12,6 +16,9 @@ impl GameAssets {
             line: Image::from_bytes(include_bytes!("../../../static/line.png"))?,
             x: Image::from_bytes(include_bytes!("../../../static/x.png"))?,
             z: Image::from_bytes(include_bytes!("../../../static/z.png"))?,
+            font: Font::from_bytes(
+                include_bytes!("../../../static/font/DeliusUnicase-Bold.ttf").to_vec(),
+            )?,
         })
     }
 
